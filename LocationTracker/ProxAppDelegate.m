0b2eed941941ce76b64593b306269750ee09b9ab
rui yang <rui.yang@proximiant.com>
2014-06-09 16:51:32 -0700

    more logging.

--- LocationTracker/ProxAppDelegate.m
+++ LocationTracker/ProxAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "ProxAppDelegate.h"
+#import "ProxUtils.h"
 
 @implementation ProxAppDelegate
 
@@ -26,6 +27,7 @@
 {
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  [ProxUtils appendToTempfile:@"linc2.log" content:[NSString stringWithFormat:@"Background-%@\n", [[NSDate date] description]]];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -36,11 +38,13 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  [ProxUtils appendToTempfile:@"linc2.log" content:[NSString stringWithFormat:@"Active-%@\n", [[NSDate date] description]]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  [ProxUtils appendToTempfile:@"linc2.log" content:[NSString stringWithFormat:@"Terminate-%@\n", [[NSDate date] description]]];
 }
 
 @end
